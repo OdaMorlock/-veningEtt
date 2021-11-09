7420bdad6ecc4d3a54fedb7351313a0db58fec91
OdaMorlock <mbmarcusmb@hotmail.com>
2021-11-09 14:16:03 +0100

    Add project files.

---
+++ CreateUser.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class CreateUser
+    {
+        public static void CreateAUser()
+        {
+
+            GetUserName getUserName = new GetUserName();
+            UserInfo userInfo = new UserInfo();
+
+            //Getting FirstName
+            Console.WriteLine("What´s your FirstName?");
+            userInfo.FirstName = getUserName.UserName();
+
+            //Getting LastName
+            Console.WriteLine("What´s your LastName?");
+            userInfo.LastName = getUserName.UserName();
+
+            //Final Error Check incase something happen and return null or Empty
+            if (!String.IsNullOrWhiteSpace(userInfo.FirstName) || !String.IsNullOrWhiteSpace(userInfo.LastName))
+            {
+
+                userInfo.UserName = getUserName.SetUserName(userInfo.FirstName, userInfo.LastName);
+
+                Console.WriteLine($"{userInfo.UserName}");
+
+            }
+
+            else
+            {
+                Console.WriteLine("Somehow First name and/or Last Name is empty");
+            }
+
+            //Save userInfo.UserName too DB
+
+        }
+    }
+}
---
+++ GetUserName.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class GetUserName
+    {
+
+        public string UserName()
+        {
+
+            var Name = Console.ReadLine();
+
+            bool containsInt = Name.Any(char.IsDigit);
+
+
+
+            //Check for Null or EmptySpace (SpaceBar)
+            if (String.IsNullOrWhiteSpace(Name))
+            {
+                Console.WriteLine("You dident write anything please try one more time");
+                Name = Console.ReadLine();
+
+                if (String.IsNullOrWhiteSpace(Name))
+                {
+                    Console.WriteLine("Program will End");
+
+                    Console.ReadLine();
+
+                    Environment.Exit(0);
+
+                }
+            }
+
+
+
+            //Check for Number in string 
+            if (containsInt)
+            {
+                Console.WriteLine("You wrote a Number Please use letters");
+                Name = Console.ReadLine();
+
+                containsInt = Name.Any(char.IsDigit);
+
+                if (containsInt)
+                {
+                    Console.WriteLine("Program will End");
+
+                    Console.ReadLine();
+
+                    Environment.Exit(0);
+
+                }
+            }
+
+
+            return Name;
+
+
+        }
+
+        public string SetUserName(string UserFirstName, string UserLastName) 
+        {
+
+            //Getting First lett from FirstName
+            var FirstLetter = UserFirstName.Substring(0, 1);
+
+            var UserName = FirstLetter + "." + UserLastName;
+
+            return UserName;
+
+        }
+
+ 
+    }
+}
